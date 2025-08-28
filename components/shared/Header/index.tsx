5865eb13cef021c32c18cce8ef66663a82775240
Techie-Mahesh <maheshTech1205@gmail.com>
2025-08-28 11:44:04 +0530

    Responsive side bar

--- components/shared/Header/index.tsx
+++ components/shared/Header/index.tsx
@@ -1,57 +1,30 @@
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { NavigationMenu } from "./types";
-import ModeToggle from "./ModeToggle";
+import Menu from "./Menu";
 
 const Header = () => {
-  const navigationMenus: NavigationMenu[] = [
-    {
-      id: "cart",
-      variant: "ghost",
-      icon: <ShoppingCart />,
-      label: "Cart",
-    },
-    {
-      id: "signIn",
-      icon: <UserIcon />,
-
-      label: "Sign In",
-      variant: "default",
-    },
-  ];
   return (
-    <header className=" border-b w-full">
+    <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/">
+          <Link href="/" className="flex-start">
             <Image
+              priority={true}
               src="/images/logo.svg"
               width={48}
               height={48}
-              alt="Prostore"
+              alt={`${APP_NAME} logo`}
             />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
           </Link>
-          <span className="hidden lg:block text-2xl ml-3">{APP_NAME}</span>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-
-          {navigationMenus.map((navItem) => (
-            <Button key={navItem.id} asChild variant={navItem.variant}>
-              <Link href={`/${navItem.id}`}>
-                {navItem.icon}
-                {navItem.label}
-              </Link>
-            </Button>
-          ))}
-        </div>
+        <Menu />
       </div>
     </header>
   );
 };
-
 export default Header;
